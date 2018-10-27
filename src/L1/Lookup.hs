5a165f714221185c12f72fcb6cd5c16907d16860
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2018-10-27 21:23:47 +0100

    Add heap/object lookups and updates

--- src/L1/Lookup.hs
+++ src/L1/Lookup.hs
@@ -1,8 +1,10 @@
 module L1.Lookup where
 
+import Data.Functor ( (<&>) )
 import qualified Data.Map as M
 
 import L1.Syntax
+import L1.Runtime
 
 field :: Program -> ClassId -> FieldId -> Type
 field p c f
@@ -15,3 +17,29 @@ fields p c
 method :: Program -> ClassId -> MethId -> Method
 method p c m
   = snd (p M.! c) M.! m
+
+objectField :: Object -> FieldId -> Value
+objectField o f
+  = snd o M.! f
+
+objectUpdate :: Object -> FieldId -> Value -> Object
+objectUpdate o f v
+  = o <&> M.insert f v
+
+heapUpdate :: Heap -> Address -> Object -> Heap
+heapUpdate h a o
+  = M.insert a o h
+
+heapObjectField :: Heap -> Address -> FieldId -> Value
+heapObjectField h a f
+  = snd (h M.! a) M.! f
+
+heapFieldUpdate :: Heap -> Address -> FieldId -> Value -> Heap
+heapFieldUpdate h a f v =
+  heapUpdate h a (objectUpdate (h M.! a) f v)
+
+stackThis :: StackFrame -> Address
+stackThis = fst
+
+stackX :: StackFrame -> Value
+stackX = snd
