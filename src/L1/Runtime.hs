2cb49a744c2365f998fced6a556e5446963b52a8
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2018-10-28 10:13:26 +0000

    Remove extra derived Eq

--- src/L1/Runtime.hs
+++ src/L1/Runtime.hs
@@ -15,16 +15,13 @@ data Value
   | FalseV
   | NullV
   | Address Address
-  deriving Eq
 
 type Object = (ClassId, M.Map FieldId Value)
 
 data Deviation
   = NullPntrExc
   | StuckErr
-  deriving Eq
 
 data Result
   = Deviation Deviation
   | Value Value
-  deriving Eq
